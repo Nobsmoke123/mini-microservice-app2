1f2b6aa98838733f09be977bacdfeb8cc1b75d08
Nobsmoke123 <donaldakobundu@gmail.com>
2025-04-26 17:44:03 +0100

    feat(events): implement event handling and enhance posts retrieval in query service

--- event-bus/src/index.js
+++ event-bus/src/index.js
@@ -12,12 +12,18 @@ app.post("/events", async (req, res) => {
 
   console.log("Event received:", event);
 
+  events.push(event);
+
   await axios.post("http://localhost:4000/events", event);
   await axios.post("http://localhost:4001/events", event);
   await axios.post("http://localhost:4002/events", event);
   await axios.post("http://localhost:4003/events", event);
 
   res.status(200).send({ status: "OK" });
+});
+
+app.get("/events", (req, res) => {
+  res.status(200).send(events);
 });
 
 app.listen(4005, () => {
--- query-service/src/index.js
+++ query-service/src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 
@@ -13,15 +14,7 @@ app.use(express.json());
 
 const posts = {};
 
-app.get("/posts", (req, res) => {
-  res.status(200).send(posts);
-});
-
-app.post("/events", (req, res) => {
-  const event = req.body;
-
-  console.log("Event received:", event);
-
+const handleEvents = (event) => {
   if (event.type === "PostCreated") {
     const { id, title } = event.data;
     posts[id] = {
@@ -52,11 +45,28 @@ app.post("/events", (req, res) => {
       }
     }
   }
-  console.log("The posts are:", posts);
+};
+
+app.get("/posts", (req, res) => {
+  const event = req.body;
+  handleEvents(event);
+  res.status(200).send(posts);
+});
+
+app.post("/events", (req, res) => {
+  const event = req.body;
+
   console.log("Event received:", event);
+
   res.status(200).send({ status: "OK" });
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
   console.log("Query service is running on port 4002");
+
+  const res = await axios.get("http://localhost:4005/events");
+
+  for (let event of res.data) {
+    handleEvents(event);
+  }
 });
