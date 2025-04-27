55205e7b27fa54a5d379b5357f783d480e09a027
Nobsmoke123 <donaldakobundu@gmail.com>
2025-04-27 19:30:50 +0100

    feat(deployment): add Kubernetes deployment and service for posts microservice

--- posts/src/index.js
+++ posts/src/index.js
@@ -42,6 +42,6 @@ app.post("/events", (req, res) => {
   res.status(200).send({ status: "OK" });
 });
 
-app.listen(4000, () => {
+app.listen(4000, "0.0.0.0", () => {
   console.log(`Server is running on port 4000`);
 });
